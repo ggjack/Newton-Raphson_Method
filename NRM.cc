5f86f22017f73827202563d0894eab7a87e08921
Kcaj Chen <Jack@v1020-wn-213-215.campus-dynamic.uwaterloo.ca>
2016-01-13 15:39:47 -0500

    Just started the program.

---
+++ NRM.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <iomanip>
+
+using namespace std;
+
+int main(int argc, char* argv[]){
+	int x0;
+	vector<string> function;
+	cout << "Please enter a value for x0, to approximate the root: " ;
+	cin >> x0;
+
+}
