3162f185c947eae63fed8b587590ab5b5b5df04d
ggjack <gg.jack@hotmail.com>
2016-01-13 22:49:13 -0500

    Finished adding inputs

--- NRM.cc
+++ NRM.cc
@@ -6,9 +6,18 @@
 using namespace std;
 
 int main(int argc, char* argv[]){
-	int x0;
+	double x0;
+	int decimal;
+	string terms;
 	vector<string> function;
+	vector
 	cout << "Please enter a value for x0, to approximate the root: " ;
 	cin >> x0;
-
+	cout << endl << "Now please enter the number of digits you want to be shown: ";
+	cin >> decimal;
+	cout << endl << "Now please enter a function, with each term separated wtih a space:" << endl;
+	while(cin >> terms){
+		function.push_back(terms);
+	}
+	
 }
